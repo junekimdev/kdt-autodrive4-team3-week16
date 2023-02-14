a7e3ec9fba6c3db965996b9b1aefa20bfc938f44
June Kim <godlyjune@gmail.com>
2023-02-14 15:55:47 +0900

    Use PCL in ROS
    - Publish type pcl::PointCloud<pcl::PointXY>
    - Filter data points

--- t3_sensor_lidar/src/t3_sensor_lidar.cpp
+++ t3_sensor_lidar/src/t3_sensor_lidar.cpp
@@ -3,6 +3,9 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 #include "sensor_msgs/PointCloud2.h"
+#include "pcl_ros/point_cloud.h"
+#include "pcl/point_types.h"
+#include "pcl_conversions/pcl_conversions.h"
 #include "laser_geometry/laser_geometry.h"
 
 namespace sensor
@@ -13,6 +16,9 @@ const std::string PUB_TOPIC = "lidar_data";
 constexpr double LASER_PROJECTION_RANGE_CUTOFF = -1.;
 constexpr int LASER_PROJECTION_OPTIONS =
     laser_geometry::channel_option::Intensity | laser_geometry::channel_option::Distance;
+constexpr float CLOUD_MIN_X = -2.f;
+constexpr float CLOUD_MIN_Y = -.5f;
+constexpr float CLOUD_MAX_Y = .5f;
 
 class Lidar
 {
@@ -24,33 +30,32 @@ class Lidar
 public:
   bool enable_debug;
 
-  Lidar() : left_detected(false), right_detected(false), lpos(0), rpos(WIDTH - 1)
+  Lidar()
   {
     node.param<bool>("sensor_lidar_enable_debug", enable_debug, true);
     sub = node.subscribe(SUB_TOPIC, 1, &Lidar::callback, this);
-    pub = node.advertise<sensor_msgs::PointCloud2>(PUB_TOPIC, 1);
-    if (enable_debug)
-      cv::namedWindow(NAME);
+    pub = node.advertise<pcl::PointCloud<pcl::PointXY>>(PUB_TOPIC, 1);
   }
 
   void callback(const sensor_msgs::LaserScan::ConstPtr& msg);
-  // void publish();
-  // void process();
+  pcl::PointCloud<pcl::PointXY> process(pcl::PointCloud<pcl::PointXY>& cloud_data);
+  void publish(pcl::PointCloud<pcl::PointXY>& msg);
 };
 
 void Lidar::callback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
   try
   {
-    sensor_msgs::PointCloud2 output;
-    output.header.stamp = ros::Time::now();
-    output.header.frame_id = NAME;
+    // Convert from sensor_msgs::LaserScan to sensor_msgs::PointCloud2
+    sensor_msgs::PointCloud2 sensor_data;
+    projector.projectLaser(*msg, sensor_data, LASER_PROJECTION_RANGE_CUTOFF, LASER_PROJECTION_OPTIONS);
 
-    projector.projectLaser(*msg, output, LASER_PROJECTION_RANGE_CUTOFF, LASER_PROJECTION_OPTIONS);
+    // Convert from sensor_msgs::PointCloud2 to pcl::PointCloud<pcl::PointXY>
+    pcl::PointCloud<pcl::PointXY> cloud_data;
+    pcl::fromROSMsg(sensor_data, cloud_data);
 
-    pub.publish(output);
-    if (enable_debug)
-      ROS_INFO("Pointcloud height: %d | width: %d", output.height, output.width);
+    pcl::PointCloud<pcl::PointXY> msg = process(cloud_data);
+    publish(msg);
   }
   catch (const std::exception& e)
   {
@@ -59,14 +64,32 @@ void Lidar::callback(const sensor_msgs::LaserScan::ConstPtr& msg)
   }
 }
 
-// void Lidar::publish(sensor_msgs::PointCloud2 msg)
-// {
-// }
+pcl::PointCloud<pcl::PointXY> Lidar::process(pcl::PointCloud<pcl::PointXY>& cloud_data)
+{
+  pcl::PointCloud<pcl::PointXY> msg;
 
-// void Lidar::process()
-// {
-//   publish();
-// }
+  // cloud_data's coordinates:
+  // +x:= back | -x:= front
+  // +y:= right | -y:= left
+  // +z:= top | -z: bottom
+  for (const auto& p : cloud_data.points)
+  {
+    if (p.y >= CLOUD_MIN_Y && p.y <= CLOUD_MAX_Y && p.x >= CLOUD_MIN_X)
+      msg.points.emplace(p);
+  }
+  return msg;
+}
+
+void Lidar::publish(pcl::PointCloud<pcl::PointXY>& msg)
+{
+  msg.header.stamp = ros::Time::now();
+  msg.header.frame_id = NAME;
+  msg.height = 1;                 // for unorganized dataset
+  msg.width = msg.points.size();  // for unorganized dataset
+  pub.publish(msg);
+  if (enable_debug)
+    ROS_INFO("Pointcloud length: %d", msg.points.size());
+}
 
 }  // namespace sensor
 
